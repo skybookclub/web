3516c85253c91371f4449648e805a19d996f81c4
Andrew Maraev <the_vk@thevk.net>
2025-01-28 21:35:11 -0800

    implement HydrateBookJob

--- app/jobs/hydrate_book_job.rb
+++ app/jobs/hydrate_book_job.rb
@@ -1,7 +1,23 @@
+require 'open-uri'
+
 class HydrateBookJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    # Do something later
+    Book.where(title: nil).find_each do |book|
+      logger.info "Hydrating book: #{book.id}"
+
+      uri = "https://www.goodreads.com/search?utf8=%E2%9C%93&query=#{book.isbn13}"
+      html_content = URI(uri).open(&:read)
+      
+      json_ld_match = html_content.match(/<script\s+type="application\/ld\+json">(.*?)<\/script>/m)
+      if json_ld_match && json_ld_match[1]
+        json_data = JSON.parse(json_ld_match[1].strip)
+        book.title = json_data["name"]
+        book.author = json_data["author"][0]["name"]
+        book.cover_image_url = json_data["image"]
+        book.save!
+      end
+    end
   end
 end
